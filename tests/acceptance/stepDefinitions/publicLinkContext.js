c7b24756c09d893aae48df0553baf1db9a727986
Ashish Bhattarai <ashish@jankaritech.com>
2019-12-19 14:08:25 +0545

    Update publicLinkContext.js

--- tests/acceptance/stepDefinitions/publicLinkContext.js
+++ tests/acceptance/stepDefinitions/publicLinkContext.js
@@ -32,7 +32,7 @@ When(
 
 When('the public uses the webUI to access the last public link created by user {string}', async function (linkCreator) {
   const lastShare = await sharingHelper.fetchLastPublicLinkShare(linkCreator)
-  console.log('Dhampus')
+  console.log('Pokhara')
   if (lastShare.permissions === sharingHelper.PERMISSION_TYPES.create) {
     return client.page.filesDropPage().navigateAndWaitTillLoaded(lastShare.token)
   }
